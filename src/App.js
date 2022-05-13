1e7b93a5476676e0b822b9b81cf7d15bae9c8863
Valerita Mollea <vale.mollea@gmail.com>
2022-05-13 20:42:21 -0300

    feat(*): add fonts and first set up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import { theme, GlobalStyles } from './style';
+import { MainPage} from './components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Vengan a nuestro casamiento</h1>
-        <img src="https://i.etsystatic.com/7613481/r/il/7cfde6/1190311025/il_fullxfull.1190311025_gj4t.jpg" alt="Espera un ratito ahi carga" />
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+       <MainPage />
+    </ThemeProvider>
   );
 }
 
---
+++ src/style/GlobalStyles.js
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Breetty';
+    src: url('/fonts/Breetty.otf') format('opentype');
+    font-weight: 400;
+    font-style: normal;
+  };
+
+  @font-face {
+    font-family: 'Quicksand';
+    src: url('/fonts/Quicksand.ttf') format('truetype');
+    font-weight: 400;
+    font-style: normal;
+  };
+
+  @font-face {
+    font-family: 'PTRegular';
+    src: url('/fonts/PTRegular.ttf') format('truetype');
+    font-weight: 400;
+    font-style: normal;
+  };
+
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    color: ${({theme}) => theme.colors.textDefault};
+    font-size: 1rem;
+    font-family: ${({theme}) => theme.fonts.default};
+  }
+
+  h1, h2, h3 {
+    font-weight: 500;
+    font-family: ${({theme}) => theme.fonts.italic};
+  }
+`;
